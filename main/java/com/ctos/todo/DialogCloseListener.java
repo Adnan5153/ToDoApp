aee4d68a477e94cff1634815e96a3984cf8e4d1b
Adnan5153 <54661857+Adnan5153@users.noreply.github.com>
2024-03-26 14:23:15 +0600

    Add files via upload

---
+++ main/java/com/ctos/todo/DialogCloseListener.java
@@ -0,0 +1,7 @@
+package com.ctos.todo;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListener {
+    public void handleDialogClose(DialogInterface dialog);
+}
---
+++ main/java/com/ctos/todo/TaskLayoutActivity.java
@@ -0,0 +1,14 @@
+package com.ctos.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaskLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.task_layout);
+    }
+}
